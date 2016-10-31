a5d89c8425ddb14953b2ff4a651b5ff0ab2a9f5b
Alexander Konovalov <alex.knvl@gmail.com>
2016-10-31 15:35:51 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='vocab',
+      version='0.1',
+      description='Vocabulary class',
+      author='Alexander Konovalov',
+      author_email='alex.knvl@gmail.com',
+      packages=['vocab'])
